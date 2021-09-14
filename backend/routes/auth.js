3fbe09079352254528162c54c7e2d1f9976b33fc
vikas <vikaspatelkherad@gmail.com>
2021-09-13 21:48:47 -0700

    creating login Endpoint & sending auth token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -17,8 +17,8 @@ router.post(
       min: 5,
     }),
   ],
-  // if there are errors, return bed request and the error
   async (req, res) => {
+    // if there are errors, return bed request and the error
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
@@ -55,6 +55,50 @@ router.post(
     } catch (error) {
       console.error(error.message);
       res.status(500).send("some error occured");
+    }
+  }
+);
+
+// login a User using POST "/api/auth/login" Login required
+
+router.post(
+  "/login",
+  [
+    body("email", "Enter a valid Email").isEmail(),
+    body("password", "Password cannot be blank").exists(),
+  ],
+  // if there are errors, return bed request and the error
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ error: "Please try to login with correct Credentials" });
+      }
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if (!passwordCompare) {
+        return res
+          .status(400)
+          .json({ error: "Please try to login with correct Credentials" });
+      }
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      const authtoken = jwt.sign(data, JWT_SECRET);
+      res.json({ authtoken });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal server error");
     }
   }
 );
