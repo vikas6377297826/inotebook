36202637fc21d30f14075373a0a9a101b0fdfefa
vikas <vikaspatelkherad@gmail.com>
2021-09-23 22:33:20 -0700

    Adding AddNote component to inotebook

--- src/components/Noteitem.js
+++ src/components/Noteitem.js
@@ -8,6 +8,8 @@ const Noteitem = (props) => {
         <div className="card-body">
           <h5 className="card-title">{note.title}</h5>
           <p className="card-text">{note.description}</p>
+          <i className="far fa-trash-alt mx-2"></i>
+          <i className="far fa-edit mx-2"></i>
         </div>
       </div>
     </div>
